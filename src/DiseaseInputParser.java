4badb51088d33d3739b019631f3690defe6c4978
voidling <eli.stonium@gmail.com>
2016-04-06 13:33:40 -0600

    WOW IT ACTUALLY WORKS

--- src/DiseaseInputParser.java
+++ src/DiseaseInputParser.java
@@ -1,3 +1,6 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Vector;
 
@@ -36,8 +39,8 @@ public class DiseaseInputParser {
         startingSusceptible = new Apfloat(inputData.get(8) - inputData.get(9));
         startingInfected = new Apfloat(inputData.get(9));
 
-        long totalXToTest = (long) ((inputData.get(2)-(inputData.get(3)))/(inputData.get(4)));
-        long totalYToTest = (long) ((inputData.get(5)-(inputData.get(6)))/(inputData.get(7)));
+        long totalXToTest = (long) ((inputData.get(3)-(inputData.get(2)))/(inputData.get(4)));
+        long totalYToTest = (long) ((inputData.get(6)-(inputData.get(5)))/(inputData.get(7)));
         if (totalXToTest < 0){
             totalXToTest =- totalXToTest;
         } if (totalYToTest < 0){
@@ -45,7 +48,7 @@ public class DiseaseInputParser {
         }
 
         long totalBufferSize = totalXToTest * totalYToTest;
-        if (totalBufferSize < (Runtime.getRuntime().maxMemory() / 32)){
+        if (totalBufferSize < (Runtime.getRuntime().maxMemory() / 128)){
             double[][] maxInfectedBuffer         = new double[(int)totalXToTest][(int)totalYToTest];
             double[][] maxInfectedTimeBuffer     = new double[(int)totalXToTest][(int)totalYToTest];
             double[][] totalInfectedBuffer       = new double[(int)totalXToTest][(int)totalYToTest];
@@ -61,26 +64,168 @@ public class DiseaseInputParser {
                         runningThreads++;
                         threadManager.add(new SIRThread(startingSusceptible, startingInfected,
                                 new Apfloat (inputData.get(2)+ (y * inputData.get(4))),
-                                new Apfloat (inputData.get(5)+ (x * inputData.get(7))), new Apfloat (inputData.get(1))));
+                                new Apfloat (inputData.get(5)+ (x * inputData.get(7))), new Apfloat (inputData.get(1)), x, y));
+                        threadManager.get(threadManager.size() - 1).start();
+
                         y++;
                     } else {
                         try {
-                            Thread.sleep(5000);
+                            Thread.sleep(20);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                             System.out.println("\nJust a heads up something went wrong while the manager was sleeping" +
                                     "\nthis was probably you interrupting it");
                         }
                     }
-                    for (int i = 0; i < threadManager.size(); i++){
+                    for (int i = threadManager.size() - 1; i >= 0; i--) {
                         if (threadManager.get(i).didComplete){
-                            Vector<>
+                            runningThreads--;
+                            totalBufferSize--;
+                            System.out.println(totalBufferSize + " operations left to complete");
+                            Vector<Apfloat> exportedVector = threadManager.get(i).returnGeneratedValues();
+                            long threadX = threadManager.get(i).x;
+                            long threadY = threadManager.get(i).y;
+
+
+                            //Finally we can convert it to doubles now that all the math is done
+                            totalInfectedBuffer[(int) threadX] [(int) threadY] = exportedVector.get(1).add(exportedVector.get(2)).doubleValue();
+                            equilibriumTimeBuffer[(int) threadX] [(int) threadY] = exportedVector.get(5).doubleValue();
+                            maxInfectedBuffer[(int) threadX] [(int) threadY] = exportedVector.get(3).doubleValue();
+                            maxInfectedTimeBuffer[(int) threadX] [(int) threadY] = exportedVector.get(4).doubleValue();
+
+                            threadManager.remove(i);
                         }
                     }
-
-
-
                 }
+            }
+
+
+
+            PrintWriter fileWriter;
+            try {
+                fileWriter = new PrintWriter("Total_Infected.csv", "UTF-8");
+                fileWriter.println("Total Infected Buffer");
+                fileWriter.print("Space Left Blank");
+
+                Double xIncrement = inputData.get(4);
+                Double xStarting  = inputData.get(2);
+
+                for (long x = 0; x < totalXToTest; x++){
+                    fileWriter.print(", " + (xStarting + (xIncrement * x)));
+                }
+                Double yIncrement = inputData.get(5);
+                Double yStarting  = inputData.get(7);
+                for (long y = 0; y < totalYToTest; y++){
+                    fileWriter.print("\n" + (yIncrement + (yIncrement * y)));
+                    for (long x = 0; x < totalXToTest; x++){
+                        fileWriter.print(", ");
+                        fileWriter.print(totalInfectedBuffer[(int) x][(int) y]);
+
+                    }
+                }
+                fileWriter.close();
+            } catch (FileNotFoundException e) {
+                System.out.println(
+                        "No idea why I can't make file Something is seriously wrong with your system");
+
+            } catch (UnsupportedEncodingException e) {
+                System.out.println(
+                        "You do not have UTF-8? I am seriously amazed");
+            }
+
+            try {
+                fileWriter = new PrintWriter("Equilibrium_Time.csv", "UTF-8");
+                fileWriter.println("Equilibrium_Time");
+                fileWriter.print("Space Left Blank");
+
+                Double xIncrement = inputData.get(4);
+                Double xStarting  = inputData.get(2);
+
+                for (long x = 0; x < totalXToTest; x++){
+                    fileWriter.print(", " + (xStarting + (xIncrement * x)));
+                }
+                Double yIncrement = inputData.get(5);
+                Double yStarting  = inputData.get(7);
+                for (long y = 0; y < totalYToTest; y++){
+                    fileWriter.print("\n" + (yIncrement + (yIncrement * y)));
+                    for (long x = 0; x < totalXToTest; x++){
+                        fileWriter.print(", ");
+                        fileWriter.print(equilibriumTimeBuffer[(int) x][(int) y]);
+
+                    }
+                }
+                fileWriter.close();
+            } catch (FileNotFoundException e) {
+                System.out.println(
+                        "No idea why I can't make file Something is seriously wrong with your system");
+
+            } catch (UnsupportedEncodingException e) {
+                System.out.println(
+                        "You do not have UTF-8? I am seriously amazed");
+            }
+
+
+            try {
+                fileWriter = new PrintWriter("Max_Infected.csv", "UTF-8");
+                fileWriter.println("Max_Infected");
+                fileWriter.print("Space Left Blank");
+
+                Double xIncrement = inputData.get(4);
+                Double xStarting  = inputData.get(2);
+
+                for (long x = 0; x < totalXToTest; x++){
+                    fileWriter.print(", " + (xStarting + (xIncrement * x)));
+                }
+                Double yIncrement = inputData.get(5);
+                Double yStarting  = inputData.get(7);
+                for (long y = 0; y < totalYToTest; y++){
+                    fileWriter.print("\n" + (yIncrement + (yIncrement * y)));
+                    for (long x = 0; x < totalXToTest; x++){
+                        fileWriter.print(", ");
+                        fileWriter.print(maxInfectedBuffer[(int) x][(int) y]);
+
+                    }
+                }
+                fileWriter.close();
+            } catch (FileNotFoundException e) {
+                System.out.println(
+                        "No idea why I can't make file Something is seriously wrong with your system");
+
+            } catch (UnsupportedEncodingException e) {
+                System.out.println(
+                        "You do not have UTF-8? I am seriously amazed");
+            }
+
+
+            try {
+                fileWriter = new PrintWriter("Max_Infected_Time.csv", "UTF-8");
+                fileWriter.println("Max Infected Time");
+                fileWriter.print("Space Left Blank, ");
+
+                Double xIncrement = inputData.get(4);
+                Double xStarting  = inputData.get(2);
+
+                for (long x = 0; x < totalXToTest; x++){
+                    fileWriter.print(", " + (xStarting + (xIncrement * x)));
+                }
+                Double yIncrement = inputData.get(5);
+                Double yStarting  = inputData.get(7);
+                for (long y = 0; y < totalYToTest; y++){
+                    fileWriter.print("\n" + (yIncrement + (yIncrement * y)));
+                    for (long x = 0; x < totalXToTest; x++){
+                        fileWriter.print(", ");
+                        fileWriter.print(maxInfectedTimeBuffer[(int) x][(int) y]);
+
+                    }
+                }
+                fileWriter.close();
+            } catch (FileNotFoundException e) {
+                System.out.println(
+                        "No idea why I can't make file Something is seriously wrong with your system");
+
+            } catch (UnsupportedEncodingException e) {
+                System.out.println(
+                        "You do not have UTF-8? I am seriously amazed");
             }
 
 
@@ -97,26 +242,40 @@ public class DiseaseInputParser {
 
 
 
+
     private Vector<String> getUserInputs () {
         Vector<String> allUserInputs = new Vector<String>(10);
 
         System.out.println("Hello and welcome to Void Plague disease calculator");
 
-        System.out.println("\nIn order, enter entropy bits, timestep size, starting and ending infection rate, infection size step, starting and ending recovery rate, and recovery timestep. Finally, total people and infected people");
+        System.out.println("\nIn order, enter 0, timestep size, starting and ending infection rate, infection size step, starting and ending recovery rate, and recovery timestep. Finally, total people and infected people");
 
         System.out.println("You can enter all these values automatically by running void-plague < input.txt");
 
-        entropy = Integer.parseInt(System.console().readLine());
 
-        for (int i = 0; i < 10; i++){
-            allUserInputs.set(i, System.console().readLine());
+        if (false) {
+
+            for (int i = 0; i < 10; i++) {
+                allUserInputs.set(i, System.console().readLine());
+            }
+        } else {
+            allUserInputs.add("0");
+            allUserInputs.add("0.1");
+            allUserInputs.add("0");
+            allUserInputs.add("5");
+            allUserInputs.add("0.1");
+            allUserInputs.add("0");
+            allUserInputs.add("5");
+            allUserInputs.add("0.1");
+            allUserInputs.add("7800000");
+            allUserInputs.add("10");
         }
 
         return allUserInputs;
     }
 
     private ArrayList<Double> convertUserInputsToSuperFloat (Vector<String> userInputs) {
-        ArrayList<Double> userInputFloatingPoints = new ArrayList<Double>;
+        ArrayList<Double> userInputFloatingPoints = new ArrayList<Double>();
         for (int i = 0; i < 10; i++){
             userInputFloatingPoints.add(Double.parseDouble(userInputs.get(i)));
         }
--- src/SIRThread.java
+++ src/SIRThread.java
@@ -1,6 +1,8 @@
 import org.apfloat.Apfloat;
 
 import java.util.Vector;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadFactory;
 
 public class SIRThread extends Thread {
     protected Apfloat susceptible;
@@ -12,49 +14,60 @@ public class SIRThread extends Thread {
     protected Apfloat timerate;
     protected Apfloat infectionRate;
     protected Apfloat recoveryRate;
-    protected Apfloat maxInfected;
-    protected Apfloat maxInfectedTimestep;
+    protected Apfloat maxInfected = new Apfloat(0);
+    protected Apfloat maxInfectedTimestep = new Apfloat(0);
 
     public boolean didComplete = false;
     public boolean removeMe = false;
 
+    protected long x = 0;
+    protected long y = 0;
+
+    private CountDownLatch countDownLatch = new CountDownLatch(1);
 
 
-    protected SIRThread (Apfloat susceptible, Apfloat infected, Apfloat infectionRate, Apfloat recoveryRate, Apfloat timerate){
-        this.susceptible = susceptible;
-        this.infected = infected;
+
+    protected SIRThread (Apfloat susceptible, Apfloat infected, Apfloat infectionRate, Apfloat recoveryRate, Apfloat timerate, long x, long y){
+        this.susceptible = susceptible.divide((susceptible.add(infected)));
+        this.infected = infected.divide((susceptible.add(infected)));
         this.infectionRate = infectionRate.multiply(timerate);
         this.recoveryRate = recoveryRate.multiply(timerate);
-        this.start();
-        try {
-            this.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-            //This could never possibly happen
-            System.out.println("HELPPPPP");
-        }
+        this.timerate = timerate;
+        this.x = x;
+        this.y = y;
 
-        didComplete = true;
+
+
     }
 
     public Vector<Apfloat> returnGeneratedValues() {
-        Vector<Apfloat> values = new Vector<Apfloat>(5);
+        Vector<Apfloat> values = new Vector<Apfloat>(6);
         values.add(this.susceptible);
         values.add(this.infected);
         values.add(this.recovered);
         values.add(this.maxInfected);
         values.add(this.maxInfectedTimestep);
+        values.add(this.timestep);
         return values;
     }
 
     private void advanceOneTimestep () {
 
+        recovered = recovered.add( (infected.multiply(recoveryRate).multiply(timerate)) );
+        infected = infected.add( (susceptible.multiply(infected.multiply(infectionRate))
+                .subtract(infected.multiply(recoveryRate))).multiply(timerate) );
+        susceptible = (new Apfloat(1)).subtract(recovered).subtract(infected);
+
+        if (infected.doubleValue() > maxInfected.doubleValue()){
+            maxInfected = infected;
+            maxInfectedTimestep = timestep;
+        }
 
 
-
-        timestep.add(timerate);
+        timestep = timestep.add(timerate);
         if (timestep.longValue() > 300){
-            this.interrupt();
+            countDownLatch.countDown();
+            didComplete = true;
         }
     }
 
@@ -63,7 +76,12 @@ public class SIRThread extends Thread {
     @Override
     public void run() {
 
-        advanceOneTimestep();
+        long startTime = System.nanoTime();
+        while (!didComplete){
+            advanceOneTimestep();
+
+        }
+        System.out.println("Time taken: " + (System.nanoTime() - startTime) + " ns");
 
     }
 
